5c435c11bf00793128037e47c9563aa308b9ef14
Steven Luo <forthworld@gmail.com>
2013-10-29 18:23:56 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiweiApp::Application.config.secret_key_base = '90ecc5a6ba4b6a4d9812d81ed973388ec88beed0c8b6f8fd72775db809b5689c5a58b20c1a9de8243243ebaf90f3260bb0562e40628ba6e4bcc954e94e327d6e'
